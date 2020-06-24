3631c4be780be182d5f38f82785cddb613611812
paul <pxl34232@gmail.com>
2020-06-24 19:29:24 -0400

    moved landing to index

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const server = http.createServer((req, res) => {
 
 let port = process.env.PORT;
 if (port == null || port == "") {
-    port = 8000;
+    port = 5000;
 }
 app.listen(port);
 server.listen(port, () => console.log(`Server running on port  ${port}`));
